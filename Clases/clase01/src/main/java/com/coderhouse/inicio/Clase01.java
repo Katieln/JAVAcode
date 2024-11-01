97df5537235672da738e9b20c12d9fb9eea442a4
Katieln <139283838+Katieln@users.noreply.github.com>
2024-11-01 06:37:26 -0500

    Tables

---
+++ Clases/clase01/src/main/java/com/coderhouse/inicio/Clase01.java
@@ -0,0 +1,32 @@
+package com.coderhouse.inicio;
+
+public class Clase01 {
+
+	public static void main(String[] args) {
+		// Declaracion de variables
+		
+		byte variableA = 10;
+		
+		byte variableB = 12;
+		
+		System.out.println(variableA);
+		
+		System.out.println("El resutlado es : " + (variableA + variableB));
+		
+		System.out.println("El resutlado No es : " + variableA + variableB);
+		
+		//Calculadora
+		
+		float num1  = 10f;
+		
+		float num2 = 12f;
+		
+		float resultado;
+		
+		resultado = num1 + num2;
+		
+		System.out.println("El resultado de la calculadora es " + resultado);
+		
+	}
+
+}
